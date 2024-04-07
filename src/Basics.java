ed58edff7321c023f7e89d5c3893da71fddb1de5
maheshnehere <maheshnehere93@gmail.com>
2024-04-08 00:03:21 +0530

    Working on java basics

---
+++ src/Basics.java
@@ -0,0 +1,27 @@
+public class Basics {
+    public static void main(String[] args) {
+        System.out.println("Hello Mahesh");
+        System.out.println(123);
+
+
+        //I am a single line comment
+
+        /* I am multi line comment
+
+         */
+
+        //data types
+
+        byte num = 1;
+        short num1 =20;
+        int num2 = 30000;
+        long num3 = 3000000;
+        float num4 = 3.24f;
+        double num5 = 4.33333d;
+        char alphabet = 'A';
+        boolean output = true;
+
+        String name = "Mahesh";
+
+    }
+}
