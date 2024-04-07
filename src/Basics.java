8428762c6c7e53bc1be564e7426fb488760fdd1b
maheshnehere <maheshnehere93@gmail.com>
2024-04-08 00:36:33 +0530

    datatypes and declartion

--- src/Basics.java
+++ src/Basics.java
@@ -13,7 +13,7 @@ public class Basics {
         //data types
 
         byte num = 1;
-        short num1 =20;
+        short num1 = 20;
         int num2 = 30000;
         long num3 = 3000000;
         float num4 = 3.24f;
@@ -21,7 +21,36 @@ public class Basics {
         char alphabet = 'A';
         boolean output = true;
 
-        String name = "Mahesh";
+        //non primitive datatype
+
+        String name = "";
+        int[] numbers = {};
+
+
+        byte[] value = {1, 2, 34, 4, 5, 6, 4};
+        short[] value1 = {1, 22, 132, 34, 4, 32};
+        int[] value2 = {1, 22, 132, 34, 4, 32};
+        long[] value3 = {1, 22, 132, 34, 4, 32};
+        float[] value4 = {1.1f, 22.4f, 132.3f};
+        double[] value5 = {1.22, 22.4, 132.4, 34.7, 4.33, 32.66};
+        char[] value6 = {'a', 's', 'f'};
+        boolean[] value7 = {true, false, false, true, false};
+
+        // declare multiple variables single line
+
+        int x = 1, y = 10, z = 100;
+
+        // declare multiple variables with single value
+
+        int number, number1, number2;
+
+        number = number1 = number2 = 10;
+
+        System.out.println(number1);
+        System.out.println(number);
+        System.out.println(number2);
+
+
 
     }
 }
