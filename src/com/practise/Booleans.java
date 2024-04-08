e72eea104d167d370264443c9389ef077f0d4753
maheshnehere <maheshnehere93@gmail.com>
2024-04-08 18:13:19 +0530

    Booleans problems

---
+++ src/com/practise/Booleans.java
@@ -0,0 +1,30 @@
+package com.practise;
+
+public class Booleans {
+    public static void main(String[] args) {
+        //method 1
+        boolean isJavafun = true;
+        boolean isfistTasty = false;
+
+        System.out.println(isJavafun);
+        System.out.println(isfistTasty);
+
+        //method 2
+
+        int x = 10;
+        int y = 20;
+
+        System.out.println(x > y);
+        System.out.println(x < y);
+        System.out.println(x == 10);
+        System.out.println(10 == 15);
+
+        //example
+
+        int myage = 24;
+        int voting = 18;
+        System.out.println("Am I eligible to vote"+" "+"= "+(myage>=voting));
+
+
+    }
+}
